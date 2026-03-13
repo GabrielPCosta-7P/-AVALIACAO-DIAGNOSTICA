35a5ccff7cff886b40b3157bcf5ad5f4f377c8fb
GabrielPCosta-7P <atoagabrielpereiracosta1402@gmail.com>
2026-03-13 00:55:52 -0300

    Add files via upload

---
+++ Atividade Diagnóstica - Questão 03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Atividade_Diagnóstica___Questão_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int maior;
+
+            Console.Write("Digite o 1º número: ");
+            maior = int.Parse(Console.ReadLine());
+
+            for (int i = 2; i <= 10; i++)
+            {
+                Console.Write("Digite o " + i + "º número: ");
+                numero = int.Parse(Console.ReadLine());
+
+                if (numero > maior)
+                {
+                    maior = numero;
+                }
+            }
+
+            Console.WriteLine("O maior número digitado foi: " + maior);
+
+            Console.ReadKey();
+        }
+    }
+}
